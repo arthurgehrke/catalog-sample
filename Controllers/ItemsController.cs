d509a4ae4882c8116a58cecc7946cedcf274e0d6
arthurgehrke <arthurgehrke07@gmail.com>
2021-09-03 12:59:24 -0300

    feat:add inicial items controller

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,41 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.Entities;
+using Catalog.Repositories;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+       private readonly InMemItemsRepository repository; 
+
+       public ItemsController()
+       {
+          repository = new InMemItemsRepository(); 
+       }
+
+       [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+
+       [HttpGet("{id}")]
+       public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+
+            if(item is null)
+            {
+                return NotFound();
+            }
+            
+            return item;
+        }
+    }
+}
